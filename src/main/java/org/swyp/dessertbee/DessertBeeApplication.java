323d3f36bd8055944bbbfb97ae954b7ce469d1e6
miu1217 <hyun671217@daum.net>
2025-01-27 00:11:13 +0900

    first commit

---
+++ src/main/java/org/swyp/dessertbee/DessertBeeApplication.java
@@ -0,0 +1,13 @@
+package org.swyp.dessertbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessertBeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DessertBeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/swyp/dessertbee/DessertBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swyp.dessertbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessertBeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
