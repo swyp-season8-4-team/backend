ab295c64b5c2f88e1594a6ef0a3a256c81b965c5
Baguette-bbang <dudals9701@naver.com>
2025-01-30 14:36:19 +0900

    feat : Security Config 설정

--- src/main/java/org/swyp/dessertbee/common/config/SecurityConfig.java
+++ src/main/java/org/swyp/dessertbee/common/config/SecurityConfig.java
@@ -1,4 +1,46 @@
 package org.swyp.dessertbee.common.config;
 
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configurable
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable()); // 개발환경에서는 잠시 꺼두기
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable()); // 추후 추가예정
+
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //oauth2
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "oauth2/**", "login/**").permitAll()
+                        .anyRequest().authenticated());
+
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
 }
