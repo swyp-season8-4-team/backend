1b99abbf1fe648ae87db27903dddb2dd0b383e63
1117mg <lee_yerim@naver.com>
2025-02-01 13:49:59 +0900

    feat: 가게 등록 기능 추가 (메뉴, 이벤트, 쿠폰 포함)

--- src/main/java/org/swyp/dessertbee/store/controller/StoreController.java
+++ src/main/java/org/swyp/dessertbee/store/controller/StoreController.java
@@ -5,9 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.swyp.dessertbee.store.dto.StoreCreateRequest;
-import org.swyp.dessertbee.store.dto.StoreResponse;
+import org.swyp.dessertbee.store.dto.request.StoreCreateRequest;
+import org.swyp.dessertbee.store.dto.response.StoreDetailResponse;
+import org.swyp.dessertbee.store.dto.response.StoreMapResponse;
+import org.swyp.dessertbee.store.dto.response.StoreSummaryResponse;
 import org.swyp.dessertbee.store.service.StoreService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/stores")
@@ -18,8 +22,29 @@ public class StoreController {
 
     /** 가게 등록 */
     @PostMapping
-    public ResponseEntity<StoreResponse> createStore(@RequestBody @Valid StoreCreateRequest request) {
+    public ResponseEntity<StoreDetailResponse> createStore(@RequestBody @Valid StoreCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(storeService.createStore(request));
+    }
+
+    /** 반경 내 가게 조회 */
+    @GetMapping("/map")
+    public List<StoreMapResponse> getStoresByLocation(
+            @RequestParam Double latitude,
+            @RequestParam Double longitude,
+            @RequestParam Double radius) {
+        return storeService.getStoresByLocation(latitude, longitude, radius);
+    }
+
+    /** 가게 간략 정보 조회 */
+    @GetMapping("/{id}/summary")
+    public StoreSummaryResponse getStoreSummary(@PathVariable Long id) {
+        return storeService.getStoreSummary(id);
+    }
+
+    /** 가게 상세 정보 조회 */
+    @GetMapping("/{id}/details")
+    public StoreDetailResponse getStoreDetails(@PathVariable Long id) {
+        return storeService.getStoreDetails(id);
     }
 
 }
--- src/main/java/org/swyp/dessertbee/store/dto/request/StoreCreateRequest.java
+++ src/main/java/org/swyp/dessertbee/store/dto/request/StoreCreateRequest.java
@@ -1,15 +1,19 @@
-package org.swyp.dessertbee.store.dto;
+package org.swyp.dessertbee.store.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.swyp.dessertbee.store.entity.StoreStatus;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -32,6 +36,14 @@ public class StoreCreateRequest {
     private Boolean parkingYn;
     private String operatingHours;
     private String closingDays;
+    private List<Long> tagIds;
     private StoreStatus status = StoreStatus.APPROVED;
-    private List<String> tags;
+
+    private List<EventCreateRequest> events;  // 이벤트 리스트
+    private List<CouponCreateRequest> coupons;  // 쿠폰 리스트
+    private List<MenuCreateRequest> menus;  // 메뉴 리스트
+
+    private List<String> storeImages;  // 대표 사진 URL 리스트
+    private Map<String, List<String>> eventImages; // 이벤트 사진 url
+    private Map<String, String> menuImages; // 메뉴 사진 url
 }
