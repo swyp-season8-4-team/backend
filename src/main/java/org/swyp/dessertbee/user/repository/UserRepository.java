19aa4e1b32e5cd9ff9ebd40ffc541b55947ed9e9
Baguette-bbang <dudals9701@naver.com>
2025-02-01 16:26:30 +0900

    feat: OAuth login 및 유저 데이터 DB 저장 기능 구현

---
+++ src/main/java/org/swyp/dessertbee/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.swyp.dessertbee.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.swyp.dessertbee.user.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    // UserEntity findByNickname(String nickname);
+    UserEntity findByEmail(String email);
+}
