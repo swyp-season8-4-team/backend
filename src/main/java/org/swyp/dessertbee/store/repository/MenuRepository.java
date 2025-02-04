9c1d27483195729bba09cae869ca88570ee5e8e0
1117mg <lee_yerim@naver.com>
2025-02-04 10:31:41 -0800

    feat: 가게별 메뉴, 이벤트 개별 CRUD

--- src/main/java/org/swyp/dessertbee/store/repository/MenuRepository.java
+++ src/main/java/org/swyp/dessertbee/store/repository/MenuRepository.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Repository;
 import org.swyp.dessertbee.store.entity.Menu;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
     // 특정 가게의 전체 메뉴 조회
     List<Menu> findByStoreId(Long storeId);
+    Optional<Menu> findByIdAndStoreId(Long menuId, Long storeId);
 }
