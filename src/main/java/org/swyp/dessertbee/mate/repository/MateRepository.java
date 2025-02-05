dfb94108e8f395694e7a210dddf7126951f80dc1
miu1217 <hyun671217@daum.net>
2025-02-06 01:45:43 +0900

    repository에 @Transactional 추가

--- src/main/java/org/swyp/dessertbee/mate/repository/MateRepository.java
+++ src/main/java/org/swyp/dessertbee/mate/repository/MateRepository.java
@@ -1,9 +1,15 @@
 package org.swyp.dessertbee.mate.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.swyp.dessertbee.mate.entity.Mate;
 
+import java.util.Optional;
+
+@Repository
 public interface MateRepository extends JpaRepository<Mate, Long> {
 
-
+    @Transactional
+    Optional<Mate> findByMateIdAndDeletedAtIsNull(Long mateId);
 }
