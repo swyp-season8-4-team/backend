7748641652ceec3ffbcb3d534cf2496403b1e441
miu1217 <hyun671217@daum.net>
2025-02-06 01:45:43 +0900

    repository에 @Transactional 추가

--- src/main/java/org/swyp/dessertbee/mate/repository/MateCategoryRepository.java
+++ src/main/java/org/swyp/dessertbee/mate/repository/MateCategoryRepository.java
@@ -3,11 +3,13 @@ package org.swyp.dessertbee.mate.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.swyp.dessertbee.mate.entity.MateCategory;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface MateCategoryRepository extends JpaRepository<MateCategory, Long> {
 
     //주어진 카테고리 ID로 해당 카테고리 이름 조회
