aee4adc2da6ada3d1b677a90cdeefb613f22b1a6
1117mg <lee_yerim@naver.com>
2025-02-23 15:50:18 +0900

    feat: 가게 수정, 삭제에 필요한 쿼리 추가

--- src/main/java/org/swyp/dessertbee/store/store/repository/StoreHolidayRepository.java
+++ src/main/java/org/swyp/dessertbee/store/store/repository/StoreHolidayRepository.java
@@ -1,6 +1,8 @@
 package org.swyp.dessertbee.store.store.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.swyp.dessertbee.store.store.entity.StoreHoliday;
@@ -11,4 +13,9 @@ public interface StoreHolidayRepository extends JpaRepository<StoreHoliday, Long
 
     @Query("SELECT h FROM StoreHoliday h WHERE h.storeId = :storeId")
     List<StoreHoliday> findByStoreId(@Param("storeId") Long storeId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM StoreHoliday h WHERE h.storeId = :storeId")
+    void deleteByStoreId(@Param("storeId") Long storeId);
 }
