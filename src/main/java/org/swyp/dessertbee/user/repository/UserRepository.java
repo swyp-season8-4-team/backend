192a9920f51eefdd83ad4369f6b0222590118d7f
1117mg <lee_yerim@naver.com>
2025-02-24 21:27:24 +0900

    fix: 유저 쿼리 수정 (uuid_to_bin)

--- src/main/java/org/swyp/dessertbee/user/repository/UserRepository.java
+++ src/main/java/org/swyp/dessertbee/user/repository/UserRepository.java
@@ -38,8 +38,8 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     /**
      * Uuid로 userId 조회 (삭제되지 않은 계정만)
      * */
-    /* @Query("SELECT u.id FROM UserEntity u WHERE FUNCTION('UUID_TO_BIN', u.userUuid) = :userUuid AND u.deletedAt IS NULL") */
-    @Query("SELECT u.id FROM UserEntity u WHERE u.userUuid = :userUuid AND u.deletedAt IS NULL")
+    @Query("SELECT u.id FROM UserEntity u WHERE FUNCTION('UUID_TO_BIN', u.userUuid) = :userUuid AND u.deletedAt IS NULL")
+    /* @Query("SELECT u.id FROM UserEntity u WHERE u.userUuid = :userUuid AND u.deletedAt IS NULL") */
     Long findIdByUserUuid(UUID userUuid);
 
     /**
