c8c315c2c346be6a6c6d063ba3e1bde289c5ccc4
Baguette-bbang <dudals9701@naver.com>
2025-03-09 20:36:25 +0900

    feat : 토큰 무효화를 provider 제한 없이 수행

--- src/main/java/org/swyp/dessertbee/auth/repository/AuthRepository.java
+++ src/main/java/org/swyp/dessertbee/auth/repository/AuthRepository.java
@@ -5,7 +5,15 @@ import org.swyp.dessertbee.auth.entity.AuthEntity;
 import org.swyp.dessertbee.user.entity.UserEntity;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface AuthRepository extends JpaRepository<AuthEntity, Integer> {
     Optional<AuthEntity> findByUserAndProvider(Optional<UserEntity> user, String provider);
+
+    /**
+     * 사용자의 모든 인증 정보 조회
+     * @param user 사용자 엔티티
+     * @return 사용자의 모든 인증 정보 목록
+     */
+    List<AuthEntity> findAllByUser(UserEntity user);
 }
