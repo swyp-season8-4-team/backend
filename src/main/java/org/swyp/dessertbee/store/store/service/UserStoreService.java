9d8ecd5a3b0e32cc3b4f9ca2d1001f2060776ec2
1117mg <lee_yerim@naver.com>
2025-03-12 00:57:49 +0900

    feat: 리스트별 저장된 가게 위치 조회 API

--- src/main/java/org/swyp/dessertbee/store/store/service/UserStoreService.java
+++ src/main/java/org/swyp/dessertbee/store/store/service/UserStoreService.java
@@ -8,6 +8,7 @@ import org.swyp.dessertbee.common.exception.BusinessException;
 import org.swyp.dessertbee.common.exception.ErrorCode;
 import org.swyp.dessertbee.common.service.ImageService;
 import org.swyp.dessertbee.store.store.dto.response.SavedStoreResponse;
+import org.swyp.dessertbee.store.store.dto.response.StoreListLocationResponse;
 import org.swyp.dessertbee.store.store.dto.response.UserStoreListResponse;
 import org.swyp.dessertbee.store.store.dto.response.UserStoreListSimpleResponse;
 import org.swyp.dessertbee.store.store.entity.SavedStore;
@@ -175,6 +176,11 @@ public class UserStoreService {
                 imageService.getImagesByTypeAndId(ImageType.STORE, store.getStoreId()),
                 savedStore.getUserPreferences()
         );
+    }
+
+    /** 리스트별 저장된 가게들의 위도, 경도 조회 */
+    public List<StoreListLocationResponse> getStoresByListId(Long listId) {
+        return savedStoreRepository.findStoresByListId(listId);
     }
 
     /** 리스트별 저장된 가게 조회 */
