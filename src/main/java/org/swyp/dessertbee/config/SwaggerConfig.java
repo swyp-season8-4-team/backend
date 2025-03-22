f56d70b4b9d4240938e9afdc6a9944eaa2386b44
Baguette-bbang <dudals9701@naver.com>
2025-03-22 19:06:14 +0900

    fix & docs : swagger Failed to fetch 오류 해결

--- src/main/java/org/swyp/dessertbee/config/SwaggerConfig.java
+++ src/main/java/org/swyp/dessertbee/config/SwaggerConfig.java
@@ -5,11 +5,17 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${SPRING_PROFILES_ACTIVE:unknown}")
+    private String activeProfile;
 
     @Bean
     public OpenAPI openAPI() {
@@ -28,10 +34,20 @@ public class SwaggerConfig {
 
         SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
                 .info(info)
                 .addSecurityItem(securityRequirement)
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", securityScheme));
+
+        // 프로덕션 환경에서만 서버 URL 설정 추가
+        if ("prod".equals(activeProfile) || "production".equals(activeProfile)) {
+            Server server = new Server();
+            server.setUrl("https://api.desserbee.com");
+            server.setDescription("Production Server");
+            openAPI.servers(List.of(server));
+        }
+
+        return openAPI;
     }
 }
