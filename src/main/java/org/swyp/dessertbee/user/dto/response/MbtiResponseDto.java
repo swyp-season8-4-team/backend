5950db030564c7a20dc7a4164ec0fb51fc6a38d7
Baguette-bbang <dudals9701@naver.com>
2025-03-28 01:34:47 +0900

    docs : mbti swagger 적용

--- src/main/java/org/swyp/dessertbee/user/dto/response/MbtiResponseDto.java
+++ src/main/java/org/swyp/dessertbee/user/dto/response/MbtiResponseDto.java
@@ -1,21 +1,27 @@
 package org.swyp.dessertbee.user.dto.response;
 
-import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-/**
- * MBTI 정보를 전달하기 위한 DTO 클래스
- */
 @Getter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Schema(description = "MBTI 응답 DTO")
 public class MbtiResponseDto {
 
+    @Schema(description = "MBTI ID", example = "1")
     private Long id;
+
+    @Schema(description = "MBTI 유형", example = "ENFP")
     private String mbtiType;
+
+    @Schema(description = "MBTI 이름", example = "에너지벌")
     private String mbtiName;
+
+    @Schema(description = "MBTI 설명", example = "신상과 트렌드를 따라가는 도전적인 디저트러버!")
     private String mbtiDesc;
 }
