56bf936841bffa820437224219a296143d33d555
Baguette-bbang <dudals9701@naver.com>
2025-03-29 02:04:26 +0900

    fix : 레디스 환경 설정 파일 하드코딩 제거 후 환경 변수 할당

--- src/main/java/org/swyp/dessertbee/config/RedisConfig.java
+++ src/main/java/org/swyp/dessertbee/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package org.swyp.dessertbee.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,9 +11,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @Value("${SPRING_DATA_REDIS_HOST}")
+    private String redisHost;
+
+    @Value("${SPRING_DATA_REDIS_PORT}")
+    private int redisPort;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
