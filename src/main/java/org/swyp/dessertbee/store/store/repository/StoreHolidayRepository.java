fa6ed2593bc87f1f60135eed4e3885406a812057
1117mg <lee_yerim@naver.com>
2025-04-01 01:18:08 +0900

    chore: 누락된 Repository 어노테이션 추가

--- src/main/java/org/swyp/dessertbee/store/store/repository/StoreHolidayRepository.java
+++ src/main/java/org/swyp/dessertbee/store/store/repository/StoreHolidayRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.swyp.dessertbee.store.store.entity.StoreHoliday;
 
 import java.util.List;
 
+@Repository
 public interface StoreHolidayRepository extends JpaRepository<StoreHoliday, Long> {
 
     @Query("SELECT h FROM StoreHoliday h WHERE h.storeId = :storeId")
