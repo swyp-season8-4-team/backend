16814a18a131ddaa2f16197a1bedb0d3ecefcf15
1117mg <lee_yerim@naver.com>
2025-04-04 01:02:58 +0900

    docs: 검색어 관련 스키마 정리

--- src/main/java/org/swyp/dessertbee/common/dto/PopularSearchResponse.java
+++ src/main/java/org/swyp/dessertbee/common/dto/PopularSearchResponse.java
@@ -1,13 +1,21 @@
 package org.swyp.dessertbee.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class PopularSearchResponse {
+    @Schema(description = "검색어", example = "케이크")
     private String keyword;
+
+    @Schema(description = "검색 횟수", example = "123")
     private int searchCount;
+
+    @Schema(description = "현재 순위", example = "3")
     private int rank;
+
+    @Schema(description = "이전 순위와의 차이", example = "-2")
     private int difference;
 }
