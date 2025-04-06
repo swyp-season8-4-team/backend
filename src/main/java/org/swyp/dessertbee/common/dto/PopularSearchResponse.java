b527f4492b7a7a804ab9ffe9efe618256ceb5c6b
1117mg <lee_yerim@naver.com>
2025-04-06 23:45:43 +0900

    docs: 검색 응답 dto 필수 필드 표시

--- src/main/java/org/swyp/dessertbee/common/dto/PopularSearchResponse.java
+++ src/main/java/org/swyp/dessertbee/common/dto/PopularSearchResponse.java
@@ -1,21 +1,27 @@
 package org.swyp.dessertbee.common.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class PopularSearchResponse {
+    @NotBlank
     @Schema(description = "검색어", example = "케이크")
     private String keyword;
 
+    @NotNull
     @Schema(description = "검색 횟수", example = "123")
     private int searchCount;
 
+    @NotNull
     @Schema(description = "현재 순위", example = "3")
     private int rank;
 
+    @NotNull
     @Schema(description = "이전 순위와의 차이", example = "-2")
     private int difference;
 }
--- src/main/java/org/swyp/dessertbee/common/dto/UserSearchHistoryDto.java
+++ src/main/java/org/swyp/dessertbee/common/dto/UserSearchHistoryDto.java
@@ -1,6 +1,8 @@
 package org.swyp.dessertbee.common.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.swyp.dessertbee.common.entity.UserSearchHistory;
@@ -10,12 +12,15 @@ import java.time.LocalDateTime;
 @Getter
 @AllArgsConstructor
 public class UserSearchHistoryDto {
+    @NotNull
     @Schema(description = "검색 기록 ID", example = "1")
     private Long id;
 
+    @NotBlank
     @Schema(description = "검색어", example = "케이크")
     private String keyword;
 
+    @NotNull
     @Schema(description = "검색어 기록 생성 시간", example = "2025-04-04T10:00:00")
     private LocalDateTime createdAt;
 
