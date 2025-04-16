47018341e5442af6f003c0daa5efedbe785ff3c0
miu1217 <hyun671217@daum.net>
2025-04-16 17:34:52 +0900

    헤더 내에 platform-type으로 나누던 버전 관리 엔드포인트로 변경

--- src/main/java/org/swyp/dessertbee/community/mate/controller/MateReplyController.java
+++ src/main/java/org/swyp/dessertbee/community/mate/controller/MateReplyController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
