50b668f1c316d70b9f05ff0f5eac043162fc78af
1117mg <lee_yerim@naver.com>
2025-04-29 13:57:33 +0900

    chore: 스케줄러 실행 로그 추가, 실행 시간 조정

--- src/main/java/org/swyp/dessertbee/store/preference/service/StoreTopTagBatchService.java
+++ src/main/java/org/swyp/dessertbee/store/preference/service/StoreTopTagBatchService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.swyp.dessertbee.store.preference.repository.StoreTopTagRepository;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -18,9 +20,9 @@ public class StoreTopTagBatchService {
         log.info("[배치] store_top_tag 초기화 시작");
         storeTopTagRepository.truncateStoreTopTag();
 
-        log.info("[배치] store_top_tag 집계 및 저장 시작");
+        log.info("[스케줄러 시작] 통계 집계 at {}", LocalDateTime.now());
         storeTopTagRepository.populateStoreTopTag();
 
-        log.info("[배치] store_top_tag 배치 완료");
+        log.info("[스케줄러 종료] 통계 집계 완료 at {}", LocalDateTime.now());
     }
 }
