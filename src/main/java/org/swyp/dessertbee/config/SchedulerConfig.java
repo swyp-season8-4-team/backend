3f017f879982b5434574dcface532408014b3ef5
1117mg <lee_yerim@naver.com>
2025-04-29 13:56:08 +0900

    chore: 스케줄러 멀티스레드풀 설정 추가

---
+++ src/main/java/org/swyp/dessertbee/config/SchedulerConfig.java
@@ -0,0 +1,21 @@
+package org.swyp.dessertbee.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(3); //todo: t2.micro 인스턴스 환경에서의 poolSize 적정선은 2~3개?
+        scheduler.setThreadNamePrefix("scheduled-task-");
+        scheduler.initialize();
+        return scheduler;
+    }
+}
