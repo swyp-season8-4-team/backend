07035a25c5c664589515a774438150aa134da0a8
1117mg <lee_yerim@naver.com>
2025-04-29 16:36:06 +0900

    fix: poolSize 2개로 조정

--- src/main/java/org/swyp/dessertbee/config/SchedulerConfig.java
+++ src/main/java/org/swyp/dessertbee/config/SchedulerConfig.java
@@ -13,7 +13,7 @@ public class SchedulerConfig {
     @Bean
     public TaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(3); //todo: t2.micro 인스턴스 환경에서의 poolSize 적정선은 2~3개?
+        scheduler.setPoolSize(2); //todo: t2.micro 인스턴스 환경에서의 poolSize 적정선은 2~3개?
         scheduler.setThreadNamePrefix("scheduled-task-");
         scheduler.initialize();
         return scheduler;
