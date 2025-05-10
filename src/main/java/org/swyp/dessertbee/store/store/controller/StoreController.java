44fa802a538d04671b7724852cbc74bdc64227a9
miu1217 <hyun671217@daum.net>
2025-05-10 22:35:03 +0900

    store requestMapping 원상복구

--- src/main/java/org/swyp/dessertbee/store/store/controller/StoreController.java
+++ src/main/java/org/swyp/dessertbee/store/store/controller/StoreController.java
@@ -33,7 +33,7 @@ import java.util.*;
 
 @Tag(name = "Store", description = "가게 관련 API")
 @RestController
-@RequestMapping("api/stores/")
+@RequestMapping("/api/stores")
 @RequiredArgsConstructor
 public class StoreController {
 
